3f11fba34463985053135ee39a7516ebdae172c3
Varun Shinde <varunshinde5113@gmail.com>
2025-06-26 23:43:24 +0530

    Add files via upload

---
+++ factorial.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class factorial {
+
+    public static long factorial(int n) {
+        if (n == 0)
+            return 1;
+        else
+            return n * factorial(n - 1);
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in); 
+        System.out.print("Enter a non-negative integer: ");
+        int number = scanner.nextInt();
+
+        if (number < 0) {
+            System.out.println("Factorial is not defined for negative numbers.");
+        } else {
+            long result = factorial(number);
+            System.out.println("Factorial of " + number + " is: " + result);
+        }
+
+        scanner.close(); 
+    }
+}
